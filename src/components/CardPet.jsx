ce737687e32ef0a02c41bd3766a64c5e5cdc70d0
Gustavo Balero <gubalero@hotmail.com>
2022-10-01 18:09:38 -0300

    ajustes

--- src/components/CardPet.jsx
+++ src/components/CardPet.jsx
@@ -1,38 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-    background-color: #ddd;
-    display: flex;
-    flex-direction: column;
-    width: 20%;
-    min-width: 250px;
-    padding: 20px;
-    border-radius: 10px;
-    border: 1px solid #ccc;
-    margin: 10px;
-
-    & > div{
-        text-align: center;
-    }
-
-    & > div > img{
-        height: 150px;
-        width: 150px;
-        object-fit: cover;
-        border-radius: 300px;
-        border: 4px solid #ED145B;
-    }
-
-    & > p{
-        font-size: 15px;
-        margin: 5px;
-    }
-
-    & > p > span{
-        font-weight: 900;
-    }
-`
+import { Container } from '../style/CardPet';
 
 function CardPet(props) {
     return (
---
+++ src/style/CardPet.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #ddd;
+    display: flex;
+    flex-direction: column;
+    width: 20%;
+    min-width: 250px;
+    padding: 20px;
+    border-radius: 10px;
+    border: 1px solid #ccc;
+    margin: 10px;
+
+    & > div{
+        text-align: center;
+    }
+
+    & > div > img{
+        height: 150px;
+        width: 150px;
+        object-fit: cover;
+        border-radius: 300px;
+        border: 4px solid #ED145B;
+    }
+
+    & > p{
+        font-size: 15px;
+        margin: 5px;
+    }
+
+    & > p > span{
+        font-weight: 900;
+    }
+`
